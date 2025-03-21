1105d685fa804204ff55b7da10598f1180d80b1f
Muaadh456 <muaadh.20232056@iit.ac.lk>
2025-03-21 21:58:18 +0530

    Updated index.js

--- AI-Pilgrimage-Planner/functions/index.js
+++ AI-Pilgrimage-Planner/functions/index.js
@@ -4,11 +4,6 @@ const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 const app = express();
 
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const express = require('express');
-const { v4: uuidv4 } = require('uuid');
-const app = express();
 
 admin.initializeApp({
   credential: admin.credential.cert(require('./serviceAccountKey.json')),
