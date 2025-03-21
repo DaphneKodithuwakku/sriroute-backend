265886badf7fa4142e9adfeaad6f0929f25d06ae
Muaadh456 <muaadh.20232056@iit.ac.lk>
2025-03-21 22:11:35 +0530

    Removed rules

--- AI-Pilgrimage-Planner/functions/.eslintrc.js
+++ AI-Pilgrimage-Planner/functions/.eslintrc.js
@@ -10,13 +10,7 @@ module.exports = {
     "eslint:recommended",
     "google",
   ],
-  rules: {
-    "no-restricted-globals": ["error", "name", "length"],
-    "prefer-arrow-callback": "error",
-    "quotes": ["error", "double", {"allowTemplateLiterals": true}],
-    "no-unused-vars": "warn",
-    "require-jsdoc": "off"
-  },
+  
   overrides: [
     {
       files: ["**/*.spec.*"],
